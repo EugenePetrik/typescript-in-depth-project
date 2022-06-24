dfe7384614acc6358e92689f052622806676ac6e
Eugene Petrik <>
2022-06-24 07:32:38 +0300

    Add interfaces

---
+++ src/interfaces.ts
@@ -0,0 +1,64 @@
+import { Category } from './enums';
+
+interface Book {
+    id: number;
+    title: string;
+    category: Category;
+    author: string;
+    available: boolean;
+    pages?: number;
+    // markDamaged?: (reason: string) => void;
+    // markDamaged?(reason: string): void;
+    markDamaged?: DamageLogger;
+};
+
+// interface Book {
+//     pages?: number;
+// };
+
+interface DamageLogger {
+    (reason: string): void;
+}
+
+interface Person {
+    name: string;
+    email: string;
+}
+
+interface Author extends Person {
+    numBooksPublished: number;
+}
+
+interface Librarian extends Person {
+    department: string;
+    assistCustomer: (custName: string, bookTitle: string) => void;
+}
+
+interface TOptions {
+    duration?: number;
+    speed?: number;
+}
+
+interface A {
+    name: string;
+    a: number;
+}
+
+interface Magazine {
+    title: string;
+    publisher: string;
+}
+
+interface ShelfItem {
+    title: string;
+}
+
+interface LibMgrCallback {
+    (err: Error | null, titles: string[] | null): void;
+}
+
+interface Callback<T> {
+    (err: Error | null, data: T | null): void;
+}
+
+export { Book, DamageLogger as Logger, Person, Author, Librarian, TOptions, A, Magazine, ShelfItem, LibMgrCallback, Callback };
